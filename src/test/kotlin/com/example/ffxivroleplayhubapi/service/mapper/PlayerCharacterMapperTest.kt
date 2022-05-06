c4d5751ef58c682f519735c4bbc3020daba2bcf0
a-szadurski <szadurski.arkadiusz@gmail.com>
2022-05-06 17:20:46 +0200

    basic mapper test

--- src/test/kotlin/com/example/ffxivroleplayhubapi/service/mapper/PlayerCharacterMapperTest.kt
+++ src/test/kotlin/com/example/ffxivroleplayhubapi/service/mapper/PlayerCharacterMapperTest.kt
@@ -1,6 +1,7 @@
 package com.example.ffxivroleplayhubapi.service.mapper
 
 import com.example.ffxivroleplayhubapi.dto.PlayerCharacterDto
+import com.example.ffxivroleplayhubapi.persistance.entity.PlayerCharacterEntity
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.from
 import org.junit.jupiter.api.Test
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [PlayerCharacterMapperImpl::class])
@@ -19,15 +21,29 @@ internal class PlayerCharacterMapperTest {
     @Test
     fun `given a dto when mapping to entity then all fields should be mapped correctly`() {
 
-        val dto = createPlayerCharacterDto()
+        val dto = createDto()
         val entity = mapper.toEntity(dto)
         assertThat(entity)
             .returns("firstName", from { it.firstName })
 
     }
 
-    private fun createPlayerCharacterDto(): PlayerCharacterDto {
+    @Test
+    fun `given an entity when mapping to model then all fields should be mapped correctly`() {
+
+        val entity = createEntity()
+        val model = mapper.toModel(entity)
+        assertThat(model)
+            .returns("firstName", from { it.firstName })
+
+    }
+
+    private fun createDto(): PlayerCharacterDto {
         return PlayerCharacterDto("firstName")
+    }
+
+    private fun createEntity(): PlayerCharacterEntity {
+        return PlayerCharacterEntity(UUID.randomUUID(), "firstName")
     }
 
 }

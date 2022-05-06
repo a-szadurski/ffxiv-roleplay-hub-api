c7549140f8f4fac64bc234713cfcf8b16f3c74ad
a-szadurski <szadurski.arkadiusz@gmail.com>
2022-05-06 14:12:48 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ffxiv-roleplay-hub-api"
---
+++ src/main/kotlin/com/example/ffxivroleplayhubapi/FfxivRoleplayHubApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ffxivroleplayhubapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FfxivRoleplayHubApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FfxivRoleplayHubApiApplication>(*args)
+}
